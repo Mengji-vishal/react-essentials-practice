da764d21399623df6727f0b77b1defd9ad9ec8ab
Mengji-vishal <vishalmengji11@gmail.com>
2025-12-14 13:11:08 +0530

    feat: Add React component structure for Header and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import img from "./assets/react-core-concepts.png";
+const react = ["shaan", "john", "doe"];
+function random(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
+  const name = react[random(react.length - 1)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={img} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {name} Fundamental React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
   );
